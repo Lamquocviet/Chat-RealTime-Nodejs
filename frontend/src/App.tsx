66c8e2a4244148425496b30e3bdf0e1b50d94ff9
Lamquocviet <lamquocviet2005.nvt@gmail.com>
2026-01-31 11:49:58 +0700

    add UseThemeStore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,29 +4,28 @@ import ChatAppPage from "./pages/ChatAppPage";
 import { Toaster } from "sonner";
 import SignUpPage from "./pages/SignUpPage";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { useEffect } from "react";
+import { useThemeStore } from "./stores/useThemeStore";
 
 function App() {
+  const { isDark, setTheme } = useThemeStore();
+
+  useEffect(() => {
+    setTheme(isDark);
+  }, [isDark]);
+
   return (
     <>
       <Toaster richColors />
       <BrowserRouter>
         <Routes>
           {/* public routes */}
-          <Route
-            path="/signin"
-            element={<SignInPage />}
-          />
-          <Route
-            path="/signup"
-            element={<SignUpPage />}
-          />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
 
           {/* protectect routes */}
           <Route element={<ProtectedRoute />}>
-            <Route
-              path="/"
-              element={<ChatAppPage />}
-            />
+            <Route path="/" element={<ChatAppPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
